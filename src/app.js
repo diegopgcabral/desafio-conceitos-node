6a7d609b14b158c6c75f89855b6d660444278661
Diego Cabral <diegopgc@gmail.com>
2020-04-15 21:15:05 -0300

    first commit

---
+++ src/app.js
@@ -0,0 +1,88 @@
+const express = require("express");
+const cors = require("cors");
+const { uuid } = require("uuidv4");
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+const repositories = [];
+
+app.get("/repositories", (request, response) => {
+  return response.status(200).json(repositories); 
+});
+
+app.post("/repositories", (request, response) => {
+  const { url, title, techs} = request.body;
+
+  const repository = {
+    id: uuid(),
+    url,
+    title,
+    techs,
+    likes: 0,
+  };
+
+  repositories.push(repository);
+
+  return response.status(201).json(repository);
+});
+
+app.put("/repositories/:id", (request, response) => {
+  const {id} = request.params;
+  const { url, title, techs} = request.body;
+
+  if(request.body.likes) {
+    return response.json({likes: 0});
+  }
+
+  const repositoryIndex = repositories.findIndex(repository => repository.id === id);
+
+  if(repositoryIndex < 0) {
+    return response.status(400).json({error: 'Repository does not exists. 😢'})
+  }
+
+  const repository = {
+    id,
+    url,
+    title,
+    techs,
+    likes: 0,
+  }
+
+  repositories[repositoryIndex] = repository;
+
+  return response.status(200).json(repository);
+});
+
+app.delete("/repositories/:id", (request, response) => {
+  const {id} = request.params;
+
+  const repositoryIndex = repositories.findIndex(repository => repository.id === id);
+
+  if(repositoryIndex < 0) {
+    return response.status(400).json({error: 'Repository does not exists. 😢'})
+  }
+
+  repositories.splice(repositoryIndex, 1);
+
+  return response.status(204).send();
+});
+
+app.post("/repositories/:id/like", (request, response) => {
+  const {id} = request.params;
+
+  const repository = repositories.find(repository => repository.id === id);
+
+  if(!repository) {
+    return response.status(400).json({error: 'Repository does not exists. 😢'})
+  }  
+
+  repository.likes += 1;
+
+  return response.status(200).json(repository);
+
+});
+
+module.exports = app;
